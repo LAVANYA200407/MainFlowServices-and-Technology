5ff09d6edef572e1bd237323986e439714343ff6
LAVANYA200407 <alavanya0703.@gmail.com>
2025-01-25 13:29:03 +0530

    Update MovieList.js

--- MovieList.js
+++ MovieList.js
@@ -1,53 +1,54 @@
-import React ,{useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import MovieItem from './MovieItem';
 
 const MovieList = () => {
-    const[SearchTerm,setSearchTerm]= useState('');
-    const[movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [movies, setMovies] = useState([]);
 
-    const handleChange = (Event) => {
-        setSearchTerm(Event.target.value);
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
     };
-     
-    const handleSubmit = async (Event) => {
-        Event.preventDefault();
-        try{ 
-            const  response=await axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=e9fb2e6b")
-            setMovies(response.data.search);
-        }
-        catch (error){
-            console.error('error');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.get("http://www.omdbapi.com/?s=Dhruva&apikey=e9fb2e6b");
+            if (response.data.Response==="True") {
+                setMovies(response.data.Search); 
+            } else {
+                setMovies([]); 
+                console.error('No movies found');
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            setMovies([]);
         }
     };
+
     return (
         <div className='container mt-4'>
-            <form onSubmit={handleSubmit}> {/* camelcase "on submit"*/ }
+            <form onSubmit={handleSubmit}>
                 <div className='input-group mb-3'>
                     <input
-                     type='text'
-                     className='form-control'
-                     placeholder='search movies...'
-                     value={SearchTerm} //corrected :"value" instead of "values"
-                     onChange={handleChange}
-                   />
-                   <button className='btn btn-outline-primary ' type='submit'> 
-                      search
+                        type='text'
+                        className='form-control'
+                        placeholder='Search movies...'
+                        value={searchTerm}
+                        onChange={handleChange}
+                    />
+                    <button className='btn btn-outline-primary' type='submit'>
+                        Search
                     </button>
-                   <div className='row'>
-                    {movies && movies.map((movie)=>(
-                        <div className='col-md-4 mb-4' key={movie.imdbID}>
-                            <MovieItem movie={movie}/>
-
-                        </div>
-
-                    ))}
-
-                   </div>
                 </div>
-
             </form>
-
+            <div className='row'>
+                {movies && movies.map((movie) => (
+                    <div className='col-md-4 mb-4' key={movie.imdbID}>
+                        <MovieItem movie={{...movie,poster:movie.poster||"https://via.placeholder.com/150" }} />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
